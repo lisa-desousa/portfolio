71e4f67ffcbed0ebf287c92e52fe954f5b39c7e9
lisa-desousa <lisa.desousamestre@gmail.com>
2026-06-09 15:39:30 +1000

    responsive padding and basic styles to contact section

--- src/components/Contact/ContactSection.tsx
+++ src/components/Contact/ContactSection.tsx
@@ -4,9 +4,9 @@ import styles from "./Contact.module.css";
 export default function ContactInfo(props: ContactInfo) {
   return (
     <section className={styles.section}>
-      <h2>Contact</h2>
+      <h2>Kontakt</h2>
       <p>Email: {props.email}</p>
-      <p>Phone: {props.phone}</p>
+      <p>Mobil: {props.phone}</p>
       <a href={props.linkedin}>LinkedIn</a>
       <a href={props.github}>GitHub</a>
     </section>
